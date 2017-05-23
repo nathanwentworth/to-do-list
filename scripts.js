af35c4a898e128e4fb33682674a348e8f950ffb2
Nathan Wentworth <nwentworth95@gmail.com>
2017-05-23 11:36:31 -0400

    started rewrite to remove jquery, jscookie

--- scripts.js
+++ scripts.js
@@ -1,50 +1,149 @@
-$(document).ready(function($) {
 
-    var regex = /\","\b/g;
-    var arr = Cookies.get('list');
-    var i = 0;
-    splitarr = 0;
-
-    if (typeof arr == 'undefined') {
-      console.log("splitarr");
-      // arr = "[]";
-      // Cookies.set('list', arr, { expires: 365, path: '' });
-    }
-    else {
-      var shortened = arr.substring(2, arr.length-2);
-      splitarr = shortened.split(regex);
-
-      console.log(splitarr);
-
-      var splitlength = splitarr.length;
-
-      if (splitlength < 4) {
-        $('ul:first-child').children('li').remove()
-      }
-      else {
-       $('ul:first-child').children('li').remove()
-
-        while (splitarr[i]) {
-          $('ul:first-child').append('<li>' + splitarr[i] + '<span><div></div><div></div></span></li>')
-          i++;
-        }
-      }
-    }
+var list;
+var listElem;
+var addField;
 
 
 
-    $('ul:first-child').sortable().draggable();
+window.onload = function () {
+  init();
+}
+
+function init() {
+  listElem = document.getElementById('list');
+  addField = document.getElementById('add');
+  addField.addEventListener('keyup', addToDoItem);
+
+  load();
+  displayList();
+}
+
+// load localstorage data
+function load() {
+  list = localStorage.getItem('list');
+  if (list == null) {
+    list = [];
+  } else {
+    list = JSON.parse(list);
+  }
+  console.log(list);
+}
+
+// debug function, deletes all stored data
+function deleteAll() {
+  localStorage.removeItem('list');
+}
+
+// save localstorage data
+function save() {
+  localStorage.setItem('list', JSON.stringify(list));
+}
+
+// write list
+function displayList() {
+  // first remove all child nodes
+  while (listElem.hasChildNodes()) {
+    listElem.removeChild(listElem.lastChild);
+  }
+
+  // then create new child nodes from the list
+  for (var i = 0; i < list.length; i++) {
+    // create the initial list node, top level parent
+    var listNode = document.createElement('li');
+    var listText = document.createTextNode(list[i]);
+    listNode.appendChild(listText);
+
+    // then create the node that will act as the x button
+    var closeNode = document.createElement('span');
+    var xNode = document.createElement('div');
+    closeNode.appendChild(xNode);
+    closeNode.appendChild(xNode);
+
+    // add the close event listener to the x node
+    closeNode.addEventListener('mouseover', displayCloseButton)
+    closeNode.addEventListener('click', removeToDoItem);
+
+    listNode.appendChild(closeNode);
+
+    // inserts the new todo item at the top of the list
+    listElem.insertBefore(listNode, listElem.firstChild);
+  }
+}
+
+// add element to the list
+function addToDoItem(event) {
+  var text = add.value;
 
 
-  $(document).on('keydown', 'input', function() {
-    $('ul:first-child').sortable().draggable();
-  });
+  if (event.keyCode == 13 && text != "") {
+    list.push(text);
+    displayList();
+    add.value = "";
+  }
 
-  $(document).on('click', '#save', function() {
-    var arr = $('ul:first-child > li').map(function() { return $(this).text() }).get();
-    console.log(arr);
-    Cookies.set('list', arr, { expires: 365, path: '' });
-  });
+  save();
+}
+
+// remove elements from the list
+function removeToDoItem() {
+
+}
+
+// displays the close 'x' on hover
+function displayCloseButton(event) {
+  console.log(event.target);
+}
+
+
+
+
+
+// $(document).ready(function($) {
+
+//   var regex = /\","\b/g;
+//   var arr = Cookies.get('list');
+//   var i = 0;
+//   splitarr = 0;
+
+//   if (typeof arr == 'undefined') {
+//     console.log("splitarr");
+//     // arr = "[]";
+//     // Cookies.set('list', arr, { expires: 365, path: '' });
+//   } else {
+//     var shortened = arr.substring(2, arr.length-2);
+//     splitarr = shortened.split(regex);
+
+//     console.log(splitarr);
+
+//     var splitlength = splitarr.length;
+
+//     if (splitlength < 4) {
+//       $('ul:first-child').children('li').remove()
+//     }
+//     else {
+//      $('ul:first-child').children('li').remove()
+
+//       while (splitarr[i]) {
+//         $('ul:first-child').append('<li>' + splitarr[i] + '<span><div></div><div></div></span></li>')
+//         i++;
+//       }
+//     }
+//   }
+
+
+
+//   $('ul:first-child').sortable().draggable();
+
+
+//   $(document).on('keydown', 'input', function() {
+//     $('ul:first-child').sortable().draggable();
+//   });
+
+//   $(document).on('click', '#save', function() {
+//     var arr = $('ul:first-child > li').map(function() { return $(this).text() }).get();
+//     console.log(arr);
+//     Cookies.set('list', arr, { expires: 365, path: '' });
+//   });
 
   // $(document).on('click', '#load', function() {
   //   // var regex = /([',[\]"])\W/g;
@@ -66,25 +165,25 @@ $(document).ready(function($) {
   //   $('ul:first-child').sortable().draggable();
   // });
 
-  $(document).on('click', 'span', function() {
-    $(this).parent('li').remove();
-  });
+//   $(document).on('click', 'span', function() {
+//     $(this).parent('li').remove();
+//   });
 
-  $(document).on('mouseover', 'ul:first-child li', function() {
-    $(this).children('span').css('visibility', 'visible');
-  });
-  $(document).on('mouseover', 'ul:first-child li span', function() {
-    $(this).css('visibility', 'visible');
-  });
-  $(document).on('mouseout', 'ul:first-child li', function() {
-    $(this).children('span').css('visibility', 'hidden');
-  });
+//   $(document).on('mouseover', 'ul:first-child li', function() {
+//     $(this).children('span').css('visibility', 'visible');
+//   });
+//   $(document).on('mouseover', 'ul:first-child li span', function() {
+//     $(this).css('visibility', 'visible');
+//   });
+//   $(document).on('mouseout', 'ul:first-child li', function() {
+//     $(this).children('span').css('visibility', 'hidden');
+//   });
 
-  $('input').keyup(function(event) {
-    var textinput = $('input').val();
-    if(event.keyCode == 13 && textinput != "") {
-      $('ul:first-child').append('<li>' + textinput + '<span><div></div><div></div></span></li>');
-      $('input[type=text]').val('');
-    }
-  });
-});
+//   $('input').keyup(function(event) {
+//     var textinput = $('input').val();
+//     if(event.keyCode == 13 && textinput != "") {
+//       $('ul:first-child').append('<li>' + textinput + '<span><div></div><div></div></span></li>');
+//       $('input[type=text]').val('');
+//     }
+//   });
+// });
